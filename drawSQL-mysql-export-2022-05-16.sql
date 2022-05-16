b20a16e8ad4f3d252aab914d7c2f6fc987460742
Angelo Melotti <96485963+callagan70@users.noreply.github.com>
2022-05-16 18:13:20 +0100

    Quasi finito

---
+++ drawSQL-mysql-export-2022-05-16.sql
@@ -0,0 +1,68 @@
+CREATE TABLE `Università de La Laguna`(
+    `id` INT UNSIGNED NULL AUTO_INCREMENT PRIMARY KEY,
+    `Indirizzo` CHAR(255) NULL COMMENT 'char(255)',
+    `N_tel` INT UNSIGNED NULL COMMENT 'not_null',
+    `web_site` INT NULL COMMENT 'not_null',
+    `Impiegati` CHAR(255) NULL,
+    `Studenti` CHAR(255) NULL
+);
+CREATE TABLE `Dipartimanti`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `Nome_Dipartimento` CHAR(255) NULL COMMENT 'Nome del corso universitario
+not_null',
+    `Descrizione` VARCHAR(255) NOT NULL COMMENT 'Descrizione corso universitario
+Null'
+);
+CREATE TABLE `Corsi di laurea`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `Nome corso` CHAR(255) NULL,
+    `Descrizione` VARCHAR(255) NOT NULL,
+    `Docenti` CHAR(255) NULL,
+    `Appelli` DATE NOT NULL
+);
+CREATE TABLE `Data Impiegati`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `Nome` CHAR(255) NULL,
+    `Cognome` CHAR(255) NULL,
+    `Data_nascita` DATE NOT NULL,
+    `Nazionalità` CHAR(255) NOT NULL,
+    `Tel` INT NOT NULL,
+    `Indirizzo` CHAR(255) NOT NULL,
+    `Ruolo` CHAR(255) NOT NULL,
+    `Stipendio` MEDIUMINT NOT NULL,
+    `Conto_accredito_stipendio` CHAR(255) NOT NULL,
+    `anno_assunzione` DATE NOT NULL
+);
+CREATE TABLE `Data Studenti`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `Nome` CHAR(255) NULL,
+    `Cognome` CHAR(255) NULL,
+    `Data di nascita` DATE NOT NULL,
+    `Nazionalità` CHAR(255) NOT NULL,
+    `Indirizzo` CHAR(255) NOT NULL,
+    `Telefono` INT NOT NULL,
+    `Tip_Documento` CHAR(255) NOT NULL,
+    `Numero_Documento` CHAR(255) NOT NULL,
+    `Corso di Laurea` CHAR(255) NOT NULL,
+    `Anno_Laurea` DATE NOT NULL,
+    `anno_iscrizione` DATE NOT NULL,
+    `Pagella` INT NOT NULL COMMENT 'Numero esami dati'
+);
+CREATE TABLE `Nazionalità`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `Italia` CHAR(255) NOT NULL,
+    `Francia` CHAR(255) NOT NULL,
+    `Spagna` CHAR(255) NOT NULL
+);
+ALTER TABLE
+    `Università de La Laguna` ADD CONSTRAINT `università de la laguna_impiegati_foreign` FOREIGN KEY(`Impiegati`) REFERENCES `Data Impiegati`(`id`);
+ALTER TABLE
+    `Università de La Laguna` ADD CONSTRAINT `università de la laguna_studenti_foreign` FOREIGN KEY(`Studenti`) REFERENCES `Data Studenti`(`id`);
+ALTER TABLE
+    `Dipartimanti` ADD CONSTRAINT `dipartimanti_nome_dipartimento_foreign` FOREIGN KEY(`Nome_Dipartimento`) REFERENCES `Università de La Laguna`(`id`);
+ALTER TABLE
+    `Dipartimanti` ADD CONSTRAINT `dipartimanti_nome_dipartimento_foreign` FOREIGN KEY(`Nome_Dipartimento`) REFERENCES `Corsi di laurea`(`id`);
+ALTER TABLE
+    `Data Impiegati` ADD CONSTRAINT `data impiegati_nazionalità_foreign` FOREIGN KEY(`Nazionalità`) REFERENCES `Nazionalità`(`id`);
+ALTER TABLE
+    `Data Studenti` ADD CONSTRAINT `data studenti_nazionalità_foreign` FOREIGN KEY(`Nazionalità`) REFERENCES `Nazionalità`(`id`);
